47b82f8575d81ccff89264fa0ca4379af9adedb7
ugurcan korkmaz <Ugurcan.korkmaz@hva.nl>
2024-09-30 16:56:09 +0200

    Galgje.py begonnen met hercoderen d.m.v. file reading

--- Kordir_Appstore/Galgje/Galgje.py
+++ Kordir_Appstore/Galgje/Galgje.py
@@ -1,69 +1,42 @@
 import random
 
-easy = open("easy.txt")
-normal = open("normal.txt")
-hard = open("hard.txt")
-impossible = open("impossible.txt")
+#bepaald door een input van welke file er een random woord van gekozen word
+def woord_kiezer(difficulty):
+    file = open(f'Difficultys/{difficulty}.txt')
+    woorden = file.readlines()
+    file.close()
 
+    randomwoorden = []
 
-print("Welkom tot galgje! Je kan kiezen tussen de volgende 4 moeilijkheden.\n "
-      "Easy , Normal , Hard , Impossible")
-
-
-def woordkieser(difficulty):
-    difficulty = difficulty + ".txt"
-    file = open(difficulty)
-    lijstwoorden = file.readlines()
-    lijstwoorden
-    lst = []
-    for woord in lijstwoorden:
-        woord.split(" ")
-        lst.append(woord)
-        print(woord)
-    print(lst)
-    woord = random.choice(lst)
-
-
+    for woord in woorden:
+        randomwoorden.append(woord)
+    woord = random.choice(randomwoorden)
     return woord
 
-def difficultysetter(difficulty):
-   if difficulty == "Easy":
-       aantalpogingen = 3
-   elif difficulty == "Normal":
-       aantalpogingen = 3
-   elif difficulty == "Hard":
-       aantalpogingen = 0
-   elif difficulty == "Impossible":
-       aantalpogingen = 0
-   return aantalpogingen
 
+#bepaald qua de moeilijkheidsgraad hoeveel pogingen de user heeft om het woord te raden
+def aantal_pogingen(difficulty):
+    #ik ga hier gebruik maken van een dictionary met als key de moeilijkheid en als value de aantal pogingen
+    return
 
-
-def game_galgje(difficulty):
-    woord = woordkieser(difficulty)
-    aantalpogingen = difficultysetter(difficulty)
-    while aantalpogingen < 6:
-        poging = input(f"Je hebt {6 - aantalpogingen} kansen om letters te onthullen!"
-                       f" Welke letter raad je?: ")
-        if poging in woord:
-            print(f"Ja! {poging} zit in het woord! Je kan nog {5 - aantalpogingen} letters onthullen")
-            aantalpogingen += 1
-        else:
-            print(f"Nee! {poging} zit niet in het woord. Je kan nog {5 - aantalpogingen} letters onthullen!")
-            aantalpogingen += 1
-        if aantalpogingen == 6:
-            pogingwoord = input("""
-Tijd is om!
-Wat is het woord?
-                         """)
-            if pogingwoord == woord:
-                print(f"Goedzo! Het woord was {woord}")
-            else:
-                print(f"GAME OVER. Het woord was {woord}")
+#main game van galgje, waar een user een willekeurig woord gekozen door woord_kiezer() kan raden.
+def galgje_game(difficulty):
+    woord = woord_kiezer(difficulty)
+    pogingen = 1
+    while True:
+        poging = input('Wat raad je?: ')
+        if poging == woord:
+            print('Je hebt gewonnen!')
+        elif pogingen == 2:
+            print('Je hebt verloren')
             break
+        else:
+            pogingen += 1
 
 
-game_galgje(input("Welke difficulty wil je?: "))
+
+
+galgje_game(input('Welke moelijkheid wil je?: '))
 
 
 
--- galgje.py
+++
@@ -1,1 +0,0 @@
-print('Hello Hasan Hoca!')
